6463c07b2aa74bc6b9576ee35b7391348d39980f
cuongnm <cuong.nguyen@amela.vn>
2022-09-19 21:44:23 +0700

    init vietnamese calendar

--- example/src/screens/calendarListScreen.tsx
+++ example/src/screens/calendarListScreen.tsx
@@ -1,102 +1,99 @@
-import React, {useState, useMemo, useCallback} from 'react';
-import {StyleSheet, Text, View, TextStyle} from 'react-native';
-import {CalendarList, DateData} from 'react-native-calendars';
+import React from 'react';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import {CalendarList, LocaleConfig} from 'react-native-calendars';
 import testIDs from '../testIDs';
 
 const RANGE = 24;
-const initialDate = '2022-07-05';
-const nextWeekDate = '2022-07-14';
-const nextMonthDate = '2022-08-05';
+const initialDate = '2022-09-18';
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
-interface Props {
-  horizontalView?: boolean;
-}
+LocaleConfig.locales['vi'] = {
+  monthNames: [
+    'Tháng 1',
+    'Tháng 2',
+    'Tháng 3',
+    'Tháng 4',
+    'Tháng 5',
+    'Tháng 6',
+    'Tháng 7',
+    'Tháng 8',
+    'Tháng 9',
+    'Tháng 10',
+    'Tháng 11',
+    'Tháng 12'
+  ],
+  monthNamesShort: [
+    'Thg 1',
+    'Thg 2',
+    'Thg 3',
+    'Thg 4',
+    'Thg 5',
+    'Thg 6',
+    'Thg 7',
+    'Thg 8',
+    'Thg 9',
+    'Thg 10',
+    'Thg 11',
+    'Thg 12'
+  ],
+  dayNames: ['Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7', 'Chủ nhật'],
+  dayNamesShort: ['Hai', 'Ba', 'Tư', 'Năm', 'Sáu', 'Bảy', 'CN']
+};
+LocaleConfig.defaultLocale = 'vi';
 
-const CalendarListScreen = (props: Props) => {
-  const {horizontalView} = props;
-  const [selected, setSelected] = useState(initialDate);
-  const marked = useMemo(() => {
-    return {
-      [nextWeekDate]: {
-        selected: selected === nextWeekDate,
-        selectedTextColor: '#5E60CE',
-        marked: true
-      },
-      [nextMonthDate]: {
-        selected: selected === nextMonthDate,
-        selectedTextColor: '#5E60CE',
-        marked: true
-      },
-      [selected]: {
-        selected: true,
-        disableTouchEvent: true,
-        selectedColor: '#5E60CE',
-        selectedTextColor: 'white'
-      }
-    };
-  }, [selected]);
-  
-  const onDayPress = useCallback((day: DateData) => {
-    setSelected(day.dateString);
-  }, []);
-
+const CalendarListScreen = () => {
   return (
     <CalendarList
       testID={testIDs.calendarList.CONTAINER}
       current={initialDate}
+      hideExtraDays={false}
       pastScrollRange={RANGE}
       futureScrollRange={RANGE}
-      onDayPress={onDayPress}
-      markedDates={marked}
-      renderHeader={!horizontalView ? renderCustomHeader : undefined}
-      calendarHeight={!horizontalView ? 390 : undefined}
-      theme={!horizontalView ? theme : undefined}
-      horizontal={horizontalView}
-      pagingEnabled={horizontalView}
-      staticHeader={horizontalView}
+      renderHeader={renderCustomHeader}
+      calendarWidth={SCREEN_WIDTH}
+      theme={theme}
+      horizontal={true}
+      pagingEnabled={true}
+      staticHeader={true}
+      firstDay={1}
     />
   );
 };
 
 const theme = {
-  stylesheet: {
-    calendar: {
-      header: {
-        dayHeader: {
-          fontWeight: '600',
-          color: '#48BFE3'
-        }
-      }
-    }
-  },
-  'stylesheet.day.basic': {
-    today: {
-      borderColor: '#48BFE3',
-      borderWidth: 0.8
+  selectedDayBackgroundColor: 'rgba(227, 47, 39, 0.15)',
+  selectedDayTextColor: '#E32F27',
+  arrowColor: '#222B45',
+  textSectionTitleColor: '#55555B',
+  todayBackgroundColor: 'rgba(227, 47, 39, 0.15)',
+  todayTextColor: '#E32F27',
+  'stylesheet.calendar.header': {
+    arrow: {
+      borderWidth: 1,
+      borderColor: 'rgba(206, 211, 222, 0.5)',
+      paddingHorizontal: 12,
+      paddingVertical: 8,
+      borderRadius: 6,
     },
-    todayText: {
-      color: '#5390D9',
-      fontWeight: '800'
-    }
+    week: {
+      backgroundColor: '#E3E3E3',
+      marginTop: 7,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      paddingTop: 4,
+    },
   }
 };
 
 function renderCustomHeader(date: any) {
-  const header = date.toString('MMMM yyyy');
-  const [month, year] = header.split(' ');
-  const textStyle: TextStyle = {
-    fontSize: 18,
-    fontWeight: 'bold',
-    paddingTop: 10,
-    paddingBottom: 10,
-    color: '#5E60CE',
-    paddingRight: 5
-  };
+  const header = date.toString('MMMM/yyyy');
+  const [month, year] = header.split('/');
 
   return (
     <View style={styles.header}>
-      <Text style={[styles.month, textStyle]}>{`${month}`}</Text>
-      <Text style={[styles.year, textStyle]}>{year}</Text>
+      <Text style={styles.month}>{`${month}`}</Text>
+      <Text style={styles.year}>{year}</Text>
     </View>
   );
 }
@@ -105,16 +102,16 @@ export default CalendarListScreen;
 
 const styles = StyleSheet.create({
   header: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-between',
-    marginTop: 10,
-    marginBottom: 10
+    alignItems: 'center'
   },
   month: {
-    marginLeft: 5
+    fontSize: 20,
+    fontWeight: '400',
+    color: '#222B45'
   },
   year: {
-    marginRight: 5
+    marginRight: 5,
+    fontSize: 12,
+    fontWeight: '400'
   }
 });
